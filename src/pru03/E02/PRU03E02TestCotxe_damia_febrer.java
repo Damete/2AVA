ea1027e5319b1975842936c2594bba9a8e580620
alumne-DAM <alumne-DAM@172.16.26.97>
2018-12-13 16:54:57 +0100

    Toma ej to guapo y su test

---
+++ src/pru03/E02/PRU03E02TestCotxe_damia_febrer.java
@@ -0,0 +1,53 @@
+package pru03.E02;
+
+public class PRU03E02TestCotxe_damia_febrer extends CotxeAbstracte implements InterfaceCotxe {
+	
+	protected EstatsMotorCotxe estat = EstatsMotorCotxe.Aturat;
+
+	public PRU03E02TestCotxe_damia_febrer(String marca, String model, TipusCanvi tipuscanvi) {
+		super(marca, model, tipuscanvi);
+	}
+
+	@Override
+	public void arrancarMotor() throws Exception {
+		if(estat.equals(EstatsMotorCotxe.Aturat)) {
+			estat = EstatsMotorCotxe.EnMarxa;
+		}
+		else {
+			throw new IllegalArgumentException("El motor ja esta enmarxa");
+		}
+
+	}
+
+	@Override
+	public EstatsMotorCotxe comprovaMotor() {
+			return estat;
+	}
+
+	@Override
+	public int getRevolucions() {
+		if(estat.equals(EstatsMotorCotxe.EnMarxa)) {
+			return (int)(Math.random() * 6500);
+		}
+		else {
+			return 0;
+		}
+	}
+
+	@Override
+	public void aturarMotor() throws Exception {
+		if(estat.equals(EstatsMotorCotxe.Aturat)) {
+			throw new IllegalArgumentException("El cotxe ja esta aturat");
+		}
+		else {
+			estat = EstatsMotorCotxe.Aturat;
+		}
+	}
+	public static void main (String[] args) throws Exception {
+		PRU03E02TestCotxe_damia_febrer PIPO = new PRU03E02TestCotxe_damia_febrer("Seat", "Ibiza", TipusCanvi.CanviManual);{
+			PIPO.arrancarMotor();
+			System.out.println(PIPO.getRevolucions());
+			System.out.println(PIPO.comprovaMotor());
+		}
+	}
+}
