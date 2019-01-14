d50c1321a5f896a09032b524f19744cf3b6710d1
Damete <Damete>
2019-01-14 20:50:23 +0100

    Cagon dioooooo

--- src/pru04/E01/PRU04E01_1_damia_febrer.java
+++ src/pru04/E01/PRU04E01_1_damia_febrer.java
@@ -12,14 +12,22 @@ import java.util.ArrayList;
 public class PRU04E01_1_damia_febrer {
 	public static void main (String[] args) {
 		try {
+			String comprovar_paraules;
+			String help = null;
 			BufferedReader br = new BufferedReader(new FileReader (args[0]));
-			BufferedWriter bw = new BufferedWriter(new FileWriter (args[0]+"_sorted.txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter (args[0] + "_sorted.txt"));
 			ArrayList<String> palabras = new ArrayList<String>();
-			for(int i=1; i<args[0].length(); i++) {
+			comprovar_paraules = br.readLine();
+			while(comprovar_paraules != null){
 				palabras.add(br.readLine());
+				if(comprovar_paraules!=null) {
+					for(int i=0; i<palabras.size(); i++) {
+						help = palabras.get(i);
+					}
+					bw.write(help + "\n");
+				}
 			}
 			Collections.sort(palabras);
-			System.out.println(palabras);
 			br.close();
 			bw.close();
 		}
