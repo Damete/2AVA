8cbfed3c434d38072b31b36a5e5b460f971ed3bb
Damete <Damete>
2019-01-15 18:44:59 +0100

    Comentarios

--- src/pru04/E01/PRU04E01_1_damia_febrer.java
+++ src/pru04/E01/PRU04E01_1_damia_febrer.java
@@ -12,19 +12,24 @@ public class PRU04E01_1_damia_febrer{
 	public static void main (String[] args) {
 		try {
 			String comprovar_paraules;
+			/* Cream els metodes per llegir i escriure al fitxer*/
 			BufferedReader br = new BufferedReader(new FileReader (args[0]));
 			BufferedWriter bw = new BufferedWriter(new FileWriter (args[0] + "sorted.txt"));
+			/*Cream un ArrayList a on es guardaran les paraules que es llegeixin del fitxer*/
 			ArrayList<String> palabras = new ArrayList<String>();
-
+			
+			/*Comprovam que el fitxer no esta buid i ficam la seguent paraula del fitxer a la seguent posicio de l'ArrayList que estigui buida*/
 			do {
 				comprovar_paraules = br.readLine();
 				if (comprovar_paraules!=null) {
 					palabras.add(comprovar_paraules);
 				}
-				
+
 			}
 			while(comprovar_paraules != null);
+			/*Ordenam les paraules alfabeticament*/
 			Collections.sort(palabras);
+			/*Escrivim les paraules al fitxer*/
 			for(int i=0; i<palabras.size(); i++) {
 				bw.write(palabras.get(i) + "\n");
 			}
