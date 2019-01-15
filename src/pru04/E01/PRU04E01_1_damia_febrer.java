e7a4daeb61b1312793cacd71cd56149b919605e1
Damete <Damete>
2019-01-15 16:05:05 +0100

    ...

--- src/pru04/E01/PRU04E01_1_damia_febrer.java
+++ src/pru04/E01/PRU04E01_1_damia_febrer.java
@@ -9,26 +9,22 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ArrayList;
 
-public class PRU04E01_1_damia_febrer {
+public class PRU04E01_1_damia_febrer{
 	public static void main (String[] args) {
 		try {
 			String comprovar_paraules;
-			String help = null;
 			BufferedReader br = new BufferedReader(new FileReader (args[0]));
-			BufferedWriter bw = new BufferedWriter(new FileWriter (args[0] + "_sorted.txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter (args[0] + "sorted"));
 			ArrayList<String> palabras = new ArrayList<String>();
-			comprovar_paraules = br.readLine();
-			while(comprovar_paraules != null){
-				palabras.add(br.readLine());
-				if(comprovar_paraules!=null) {
-					for(int i=0; i<palabras.size(); i++) {
-						help = palabras.get(i);
-					}
-					bw.write(help + "\n");
-				}
+			do {
+				comprovar_paraules = br.readLine();
+				palabras.add(comprovar_paraules);
 			}
-			Collections.sort(palabras);
+			while(comprovar_paraules != null);
 			br.close();
+			for(int i=0; i<palabras.size(); i++) {
+				bw.write(palabras.get(i));
+			}
 			bw.close();
 		}
 		catch(IOException e){
