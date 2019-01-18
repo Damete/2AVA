c52eb9a230b213a3d3741b0c57f55436b883f690
Damete <Damete>
2019-01-18 16:53:49 +0100

    Cosa

--- src/pru04/E02/PRU04E02_damia_febrer.java
+++ src/pru04/E02/PRU04E02_damia_febrer.java
@@ -1,16 +1,18 @@
-package pru04.E02;
-
 import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class PRU04E02_damia_febrer {
-	public String trocejarCamp(String cadena, int posicion_inicial, int posicion_final ) {
+	public static String trocejarCamp(String cadena, int posicion_inicial, int posicion_final ) {
 		cadena = "";
-		return cadena;
+		for (int i = posicion_inicial; i<posicion_final; i++ ) {
+			if(cadena.charAt(i) != 32) {
+				cadena += cadena.charAt(i);
+			}
+		}
+		return cadena + ";";
 	}
 	public static void main (String[] args) {
 		try {
@@ -19,9 +21,16 @@ public class PRU04E02_damia_febrer {
 			String emp_no,birth_date,first_name,last_name,gender,hire_date,leer;
 			do {
 				leer = br.readLine();
-				emp_no = br.readLine();
+				emp_no = trocejarCamp(leer,1,6);
+				birth_date = trocejarCamp(leer,7,16);
+				first_name = trocejarCamp(leer,17,27);
+				last_name = trocejarCamp(leer,28,41);
+				gender = trocejarCamp(leer,42,48);
+				hire_date = trocejarCamp(leer,49,58);
 			}
 			while(leer != null);
+			br.close();
+			bw.close();
 		}
 		catch(IOException e){
 			System.out.println("Hi ha un error");
