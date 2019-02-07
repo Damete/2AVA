eab1bb5e12818f7d70f091cbdd090d9d2dd429fe
Damete <damete>
2019-02-07 11:37:02 +0100

    test leer matricula

--- src/pru04/E05/PRU04E05Parkingtest_damia_febrer.java
+++ src/pru04/E05/PRU04E05Parkingtest_damia_febrer.java
@@ -1,5 +1,25 @@
 package pru04.E05;
 
-public class PRU04E05Parkingtest_damia_febrer {
+import java.util.Scanner;
 
+public class PRU04E05Parkingtest_damia_febrer {
+	public static void main (String[] args) {
+		System.out.println("Introduce matricula");
+		Scanner sc = new Scanner(System.in);
+		String inicial;
+		int test;
+		String matricula="";
+		inicial=sc.nextLine();
+		try {
+			for(int i=0; i<4; i++) {
+				if(inicial.charAt(i)>=48 || inicial.charAt(i)<=57) {
+					matricula+=inicial.charAt(i);
+				}
+			}
+			System.out.println(matricula);
+		}
+		catch(Exception e) {
+			System.out.println("La matricula introducida es incorrecta");
+		}
+	}
 }
