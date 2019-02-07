7ecf6cf05b96c4d911ea59aaf5b33eedea92b29f
Damete <damete>
2019-02-07 13:41:12 +0100

    Is this OK ?

--- src/pru04/E05/PRU04E05Parkingtest_damia_febrer.java
+++ src/pru04/E05/PRU04E05Parkingtest_damia_febrer.java
@@ -7,17 +7,21 @@ public class PRU04E05Parkingtest_damia_febrer {
 		System.out.println("Introduce matricula");
 		Scanner sc = new Scanner(System.in);
 		String inicial;
-		int test;
 		String matricula="";
 		inicial=sc.nextLine();
 		try {
 			for(int i=0; i<4; i++) {
-				if(inicial.charAt(i)>=48 || inicial.charAt(i)<=57) {
+				if(inicial.charAt(i)>=48 && inicial.charAt(i)<=57) {
 					matricula+=inicial.charAt(i);
 				}
 			}
 			System.out.println(matricula);
-		}
+			for(int i=4; i<7; i++) {
+				if(inicial.charAt(i)>=65 && inicial.charAt(i)<=90) {
+					matricula+=inicial.charAt(i);
+				}
+			}
+			System.out.println(matricula);		}
 		catch(Exception e) {
 			System.out.println("La matricula introducida es incorrecta");
 		}
