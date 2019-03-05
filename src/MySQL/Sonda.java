027276f5165947fe52ac4a3265a4a44ccabe98d7
Damete <Damete>
2019-03-05 15:59:56 +0100

    awuita

--- src/MySQL/Sonda.java
+++ src/MySQL/Sonda.java
@@ -1,6 +1,12 @@
 package MySQL;
 
+import java.sql.Date;
+
 public abstract class Sonda {
-	public boolean enregistarTemperatura(String IP, String Ubicacio, double valor);
-	public double obtenerTemperatura(String Ubicacio, Date data);
+	public boolean enregistarTemperatura(String IP, String Ubicacio, double valor) {
+		return false;
+	}
+	public double obtenerTemperatura(String Ubicacio, Date data) {
+		return 0;
+	}
 }
